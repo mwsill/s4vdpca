dbaddab0353f10cf93f1e649436ae04349cad418
mwsill <martin@dkfz-vpn2.inet.dkfz-heidelberg.de>
2014-10-20 16:48:40 +0200

    weakness 1-para

--- R/randomised_lasso.R
+++ R/randomised_lasso.R
@@ -1,5 +1,5 @@
 randomised_lasso <- function(ols,lambda,weakness){
-  lambda <- lambda/runif(length(ols),weakness,1)
+  lambda <- lambda/runif(length(ols),1-weakness,1)
   sign(ols) * (abs(ols) >= lambda) * (abs(ols) - lambda)
 } 
 
